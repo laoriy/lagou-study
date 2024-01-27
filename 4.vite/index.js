2219b7ef9f1b74b0ba606c8e8bad9106141b42de
laoriy <1161612005@qq.com>
2024-01-27 22:04:28 +0800

    feat: vite summary

--- 4.vite/index.js
+++ 4.vite/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const Koa = require("koa");
+const { Readable } = require("stream");
 const send = require("koa-send");
 const path = require("path");
 const compiler = require("@vue/compiler-sfc");
@@ -13,6 +14,12 @@ const streamToString = (stream) =>
     stream.on("error", reject);
     stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
   });
+const stringToStream = (text) => {
+  const stream = new Readable();
+  stream.push(text);
+  stream.push(null);
+  return stream;
+};
 
 // 3.加载第三方模块
 app.use(async (ctx, next) => {
@@ -40,7 +47,25 @@ app.use(async (ctx, next) => {
 app.use(async (ctx, next) => {
   if (ctx.path.endsWith(".vue")) {
     const { descriptor } = compiler.parse(await streamToString(ctx.body));
-    ctx.body = descriptor.template.content;
+    let code;
+
+    if (!ctx.query.type) {
+      code = descriptor.template.content;
+      code = code.replace(/export\s+default\s+/g, "const __script = ");
+      code += `
+      import { render as __render } from "${ctx.path}?type=template"
+      __script.render = __render
+      export default __script
+      `;
+    } else if (ctx.query.type === "template") {
+      const templateRender = compiler.compileTemplate({
+        source: descriptor.template.content,
+      });
+      code = templateRender.code;
+    }
+
+    ctx.type = "application/javascript";
+    ctx.body = stringToStream(code);
   }
   await next();
 });
@@ -50,7 +75,9 @@ app.use(async (ctx, next) => {
   if (ctx.type === "application/javascript") {
     const contents = await streamToString(ctx.body);
     // import Vue from 'vue' ===> import Vue from '/@modules/vue'
-    ctx.body = contents.replace(/from\s+["'](?!\.\/)/g, "$1/@modules/");
+    ctx.body = contents
+      .replace(/(from\s+['"])(?![\.\/])/g, "$1/@modules/")
+      .replace(/process\.env\.NODE_ENV/g, '"development"');
   }
   await next();
 });
