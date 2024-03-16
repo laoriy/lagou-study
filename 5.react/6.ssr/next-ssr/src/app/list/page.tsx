689d8ca087a80d9b701fd91a648100bc92a9fcd9
laoriy <1161612005@qq.com>
2024-03-16 22:50:57 +0800

    feat: next start

--- 5.react/6.ssr/next-ssr/src/app/list/page.tsx
+++ 5.react/6.ssr/next-ssr/src/app/list/page.tsx
@@ -1,7 +1,15 @@
+import type { Metadata } from 'next'
+
 export default function List() {
     return (
         <div>
-            <h1>List</h1>
+            <h1 className="flex  text-red-500 size-20 text-4xl">List</h1>
         </div>
     );
 }
+
+ 
+export const metadata: Metadata = {
+    title: 'list page',
+    description: '...',
+  }
--- 5.react/6.ssr/next-ssr/src/app/page.tsx
+++ 5.react/6.ssr/next-ssr/src/app/page.tsx
@@ -1,8 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+import type { Metadata } from 'next'
+import { readFile } from 'fs';
+import { promisify } from 'util';
+import { join } from 'path';
+const read = promisify(readFile);
 
-export default function Home() {
+export default async function Home() {
+  const {data} = await getProjects()
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex  flex-col items-center text-center justify-center p-24">
       <h1>Hello, Next.js!</h1>
+      <p>link</p>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/list">list page</Link>
+        </li>
+        <p>静态资源：</p>
+        <Image src="/vercel.svg" width={200} height={200} alt="" />
+      </ul>
+      <div>{data}</div>
     </main>
   );
+}
+
+ 
+export const metadata: Metadata = {
+  title: 'home page',
+  description: '...',
+}
+
+export async function getProjects () {
+  let data = await read(join(process.cwd(), 'src/app', 'layout.tsx'), 'utf-8');
+  
+  console.log('HELLO');
+  return {
+    data
+  }
 }
