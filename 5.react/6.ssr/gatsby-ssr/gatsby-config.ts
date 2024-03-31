adb5dd8778ac8eb1bd45409df1b5a4f3b6b2a5b8
laoriy <1161612005@qq.com>
2024-03-31 23:11:52 +0800

    feat：add json

--- 5.react/6.ssr/gatsby-ssr/gatsby-config.ts
+++ 5.react/6.ssr/gatsby-ssr/gatsby-config.ts
@@ -3,13 +3,25 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Laoriy Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-source-filesystem`, //用于将本地文件中的数据添加至数据层
+      options: {
+        // The unique name for each instance
+        name: `json`,
+        // Path to the directory
+        path: `${__dirname}/json/`,
+      },
+    },
+    "gatsby-transformer-json", //将原始 JSON 字符串转换成 JavaScript 对象
+  ],
 };
 
 export default config;
