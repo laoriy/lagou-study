c9df54f823b626bcbe5b0a45e5028b792c0d19b5
laoriy <1161612005@qq.com>
2024-05-07 23:22:01 +0800

    feat:new project

---
+++ 5.react/9.react-table/src/Table.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Table() {
+  return (
+    <table className="gridtable">
+      <tr>
+        <th>信息标题 1</th>
+        <th>信息标题 2</th>
+        <th>信息标题 3</th>
+      </tr>
+      <tr>
+        <td>文本 1A</td>
+        <td>文本 1B</td>
+        <td>文本 1C</td>
+      </tr>
+      <tr>
+        <td>文本 2A</td>
+        <td>文本 2B</td>
+        <td>文本 2C</td>
+      </tr>
+    </table>
+  );
+}
+
+export default Table;
