b3403f6e8664eff83d6a250bc763c49ff967d385
laoriy <1161612005@qq.com>
2024-05-09 23:18:24 +0800

    feat:sort

--- 5.react/9.react-table/src/Table.tsx
+++ 5.react/9.react-table/src/Table.tsx
@@ -1,52 +1,86 @@
 import React from "react";
-import { columns } from "./columns";
+import { COLUMNS, COLUMNS_GROUPS } from "./columns";
 import MockData from "./MOCK_DATA.json";
 import {
   useReactTable,
   getCoreRowModel,
-  flexRender
+  flexRender,
+  getSortedRowModel,
+  SortingState,
 } from "@tanstack/react-table";
 
 function Table() {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
   const table = useReactTable({
-    columns,
+    columns: COLUMNS_GROUPS,
     data: MockData,
     getCoreRowModel: getCoreRowModel(), //row model
+    getSortedRowModel: getSortedRowModel(), //client-side sorting
+    onSortingChange: setSorting, //optionally control sorting state in your own scope for easy access
+    state: {
+      sorting,
+    },
   });
 
-  console.log(table);
-
   return (
-    <table className="gridtable">
-      <thead>
-        {table.getHeaderGroups().map((headerGroup) => {
-          return (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map(
-                (
-                  header // map over the headerGroup headers array
-                ) => (
-                  <th key={header.id} colSpan={header.colSpan}>
-                    {flexRender(header.column.columnDef.header, header.getContext())}
-                  </th>
-                )
-              )}
+    <>
+      <pre>{JSON.stringify(sorting, null, 2)}</pre>
+      <table className="gridtable">
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => {
+            return (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map(
+                  (
+                    header // map over the headerGroup headers array
+                  ) => (
+                    <th
+                      key={header.id}
+                      colSpan={header.colSpan}
+                      onClick={header.column.getToggleSortingHandler()}
+                    >
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                      {{
+                        asc: " 🔼",
+                        desc: " 🔽",
+                      }[header.column.getIsSorted() as string] ?? null}
+                    </th>
+                  )
+                )}
+              </tr>
+            );
+          })}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
             </tr>
-          );
-        })}
-      </thead>
-      <tbody>
-        {table.getRowModel().rows.map((row) => (
-          <tr key={row.id}>
-            {row.getVisibleCells().map((cell) => (
-              <td key={cell.id}>
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+          ))}
+        </tbody>
+        <tfoot>
+          {table.getFooterGroups().map((footerGroup) => (
+            <tr key={footerGroup.id}>
+              {footerGroup.headers.map((header) => (
+                <th key={header.id} colSpan={header.colSpan}>
+                  {flexRender(
+                    header.column.columnDef.footer,
+                    header.getContext()
+                  )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+    </>
   );
 }
 
--- 5.react/9.react-table/src/columns.ts
+++ 5.react/9.react-table/src/columns.ts
@@ -1,26 +1,79 @@
-export const columns = [
+export const COLUMNS = [
   {
     header: "ID",
+    footer: "ID",
     accessorKey: "id",
   },
   {
     header: "名",
+    footer: "名",
     accessorKey: "first_name",
   },
   {
     header: "姓",
+    footer: "姓",
     accessorKey: "last_name",
   },
   {
     header: "出生日期",
+    footer: "出生日期",
     accessorKey: "date_of_birth",
   },
   {
     header: "国家",
+    footer: "国家",
     accessorKey: "country",
   },
   {
     header: "电话",
+    footer: "电话",
     accessorKey: "phone",
+  },
+];
+
+//表格分组
+export const COLUMNS_GROUPS = [
+  {
+    header: "ID",
+    footer: "ID",
+    accessorKey: "id",
+  },
+  {
+    header: "姓名",
+    footer: "姓名",
+    columns: [
+      {
+        header: "名",
+        footer: "名",
+        accessorKey: "first_name",
+      },
+      {
+        header: "姓",
+        footer: "姓",
+        accessorKey: "last_name",
+      },
+    ],
+  },
+
+  {
+    header: "一般信息",
+    footer: "一般信息",
+    columns: [
+      {
+        header: "出生日期",
+        footer: "出生日期",
+        accessorKey: "date_of_birth",
+      },
+      {
+        header: "国家",
+        footer: "国家",
+        accessorKey: "country",
+      },
+      {
+        header: "电话",
+        footer: "电话",
+        accessorKey: "phone",
+      },
+    ],
   },
 ];
