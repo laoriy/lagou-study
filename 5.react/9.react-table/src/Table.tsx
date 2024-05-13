2c31d4b62190b0f89830d10a73ccc2258d55c1b8
laoriy <1161612005@qq.com>
2024-05-13 23:11:56 +0800

    feat:列隐藏显示 改顺序

--- 5.react/9.react-table/src/Table.tsx
+++ 5.react/9.react-table/src/Table.tsx
@@ -1,4 +1,10 @@
-import React, { useCallback, useState } from "react";
+import React, {
+  HTMLProps,
+  forwardRef,
+  useCallback,
+  useMemo,
+  useState,
+} from "react";
 import { COLUMNS, COLUMNS_GROUPS } from "./columns";
 import MockData from "./MOCK_DATA.json";
 import {
@@ -9,8 +15,31 @@ import {
   SortingState,
   getFilteredRowModel,
   getPaginationRowModel,
+  ColumnOrderState,
 } from "@tanstack/react-table";
 import { debounce } from "lodash-es";
+
+function IndeterminateCheckbox({
+  indeterminate,
+  className = "",
+  ...rest
+}: { indeterminate?: boolean } & HTMLProps<HTMLInputElement>) {
+  const ref = React.useRef<HTMLInputElement>(null!);
+
+  React.useEffect(() => {
+    if (typeof indeterminate === "boolean") {
+      ref.current.indeterminate = !rest.checked && indeterminate;
+    }
+  }, [indeterminate, rest.checked]);
+  return (
+    <input
+      type="checkbox"
+      ref={ref}
+      className={className + " cursor-pointer"}
+      {...rest}
+    />
+  );
+}
 
 const GlobalFilter = ({ setFilter }: any) => {
   const onChange = debounce((value) => {
@@ -35,8 +64,47 @@ function Table() {
     pageIndex: 0, //initial page index
     pageSize: 10, //default page size
   });
+
+  const [columnVisibility, setColumnVisibility] = React.useState({});
+  const [columnOrder, setColumnOrder] = React.useState<ColumnOrderState>([]);
+
+  const columns = useMemo(
+    () => [
+      {
+        header: " ",
+        columns: [
+          {
+            id: "select",
+            header: ({ table }: any) => (
+              <IndeterminateCheckbox
+                {...{
+                  checked: table.getIsAllRowsSelected(),
+                  indeterminate: table.getIsSomeRowsSelected(),
+                  onChange: table.getToggleAllRowsSelectedHandler(),
+                }}
+              />
+            ),
+            cell: ({ row }: any) => (
+              <div className="px-1">
+                <IndeterminateCheckbox
+                  {...{
+                    checked: row.getIsSelected(),
+                    disabled: !row.getCanSelect(),
+                    indeterminate: row.getIsSomeSelected(),
+                    onChange: row.getToggleSelectedHandler(),
+                  }}
+                />
+              </div>
+            ),
+          },
+        ],
+      },
+      ...COLUMNS_GROUPS,
+    ],
+    []
+  );
   const table = useReactTable({
-    columns: COLUMNS_GROUPS,
+    columns,
     data: MockData,
     getCoreRowModel: getCoreRowModel(), //row model
     getSortedRowModel: getSortedRowModel(), //client-side sorting
@@ -45,14 +113,61 @@ function Table() {
     getColumnCanGlobalFilter: (column) => column.id !== "id",
     getPaginationRowModel: getPaginationRowModel(), //load client-side pagination code
     onPaginationChange: setPagination, //update the pagination state when internal APIs mutate the pagination state
+    onColumnVisibilityChange: setColumnVisibility,
+    onColumnOrderChange: setColumnOrder,
     state: {
       sorting,
       pagination,
+      columnOrder,
+      columnVisibility,
     },
   });
 
+  const randomizeColumns = () => {
+    const ids = table.getAllLeafColumns().map((d) => d.id);
+    ids.sort(() => Math.random() - 0.5);
+
+    table.setColumnOrder(ids);
+  };
+
   return (
     <>
+      <div className="inline-block border border-black shadow rounded">
+        <div className="px-1 border-b border-black">
+          <label>
+            <input
+              {...{
+                type: "checkbox",
+                checked: table.getIsAllColumnsVisible(),
+                onChange: table.getToggleAllColumnsVisibilityHandler(),
+              }}
+            />{" "}
+            Toggle All
+          </label>
+        </div>
+        {table.getAllLeafColumns().map((column) => {
+          return (
+            <div key={column.id} className="px-1">
+              <label>
+                <input
+                  {...{
+                    type: "checkbox",
+                    checked: column.getIsVisible(),
+                    onChange: column.getToggleVisibilityHandler(),
+                  }}
+                />{" "}
+                {column.id}
+              </label>
+            </div>
+          );
+        })}
+      </div>
+      <div className="h-4" />
+      <div className="flex flex-wrap gap-2">
+        <button onClick={() => randomizeColumns()} className="border p-1">
+          Shuffle Columns
+        </button>
+      </div>
       <GlobalFilter setFilter={table.setGlobalFilter} />
       <table className="gridtable">
         <thead>
@@ -101,20 +216,20 @@ function Table() {
             </tr>
           ))}
         </tbody>
-        {/* <tfoot>
-          {table.getFooterGroups().map((footerGroup) => (
-            <tr key={footerGroup.id}>
-              {footerGroup.headers.map((header) => (
-                <th key={header.id} colSpan={header.colSpan}>
-                  {flexRender(
-                    header.column.columnDef.footer,
-                    header.getContext()
-                  )}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </tfoot> */}
+        <tfoot>
+          <tr>
+            <td className="p-1">
+              <IndeterminateCheckbox
+                {...{
+                  checked: table.getIsAllPageRowsSelected(),
+                  indeterminate: table.getIsSomePageRowsSelected(),
+                  onChange: table.getToggleAllPageRowsSelectedHandler(),
+                }}
+              />
+            </td>
+            <td colSpan={20}>Page Rows ({table.getRowModel().rows.length})</td>
+          </tr>
+        </tfoot>
       </table>
       <div className="flex items-center gap-2">
         <button
@@ -176,6 +291,10 @@ function Table() {
             </option>
           ))}
         </select>
+      </div>
+      <div>
+        <label>Row Selection State:</label>
+        <pre>{JSON.stringify(table.getState().rowSelection, null, 2)}</pre>
       </div>
     </>
   );
