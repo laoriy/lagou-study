b3965773a35c59fb0b84e3a44c7aa9ad0fdc7a6b
laoriy <1161612005@qq.com>
2024-05-30 23:11:23 +0800

    fs end

--- 6.node/core/03fs/06-fs-dir-api.js
+++ 6.node/core/03fs/06-fs-dir-api.js
@@ -1,29 +1,29 @@
 const fs = require('fs')
 
 // 一、access 
-/* fs.access('a.txt', (err) => {
+fs.access('data.txt', (err) => {
   if (err) {
     console.log(err)
   } else {
     console.log('有操作权限')
   }
-}) */
+})
 
 // 二、stat 
-/* fs.stat('a.txt', (err, statObj) => {
-  console.log(statObj.size)
+fs.stat('data.txt', (err, statObj) => {
+  console.log(statObj)
   console.log(statObj.isFile())
   console.log(statObj.isDirectory())
-}) */
+})
 
 // 三、mkdir 
-/* fs.mkdir('a/b/c', {recursive: true}, (err) => {
+fs.mkdir('a/b/c', {recursive: true}, (err) => {
   if (!err) {
     console.log('创建成功')
   }else{
     console.log(err)
   }
-}) */
+})
 
 // 四、rmdir
 fs.rmdir('a', {recursive: true}, (err) => {
@@ -35,12 +35,12 @@ fs.rmdir('a', {recursive: true}, (err) => {
 })
 
 // 五、readdir 
-/* fs.readdir('a/b', (err, files) => {
+fs.readdir('a/b', (err, files) => {
   console.log(files)
-}) */
+})
 
 // 六、unlink
-/* fs.unlink('a/a.txt', (err) => {
+/* fs.unlink('a/d.txt', (err) => {
   if (!err) {
     console.log('删除成功')
   }
