cce4a16e2128734ca70878f614a807d516bc2d83
laoriy <1161612005@qq.com>
2024-07-30 23:24:32 +0800

    redis test

--- 6.node/redis/index.mjs
+++ 6.node/redis/index.mjs
@@ -1,15 +1,39 @@
 import { createClient } from "redis";
 
 const client = createClient({
-  url: "redis://120.79.66.173:6379",
+  url: "redis://120.79.66.173:6379", //  redis-server --daemonize yes --protected-mode no
 });
 
-client.on("error", (err) => console.log("Redis Client Error", err));
+client.on("error", (err) => console.log("client Client Error", err));
 client.on("connect", () => {
   console.log("connect...");
 });
+
 await client.connect();
 
-console.log(client);
+// 2. 操作 client 数据库
+await client.set("foo", "bar11");
 
-await client.disconnect();
+const v = await client.get("foo");
+
+await client.hSet("user-session:123", {
+  name: "John",
+  surname: "Smith",
+  company: "Redis",
+  age: 29,
+});
+
+let userSession = await client.hGetAll("user-session:123");
+console.log(JSON.stringify(userSession, null, 2));
+
+// 事务
+
+await client.set("another-key", "another-value");
+
+const [setKeyReply, otherKeyValue] = await client
+  .multi()
+  .set("key", "value")
+  .get("another-key")
+  .exec(); // ['OK', 'another-value']
+
+console.log(setKeyReply, otherKeyValue);
