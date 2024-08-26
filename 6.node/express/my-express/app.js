19d0e409db7261cadc262658d510f9cffe2b3d4c
laoriy <1161612005@qq.com>
2024-08-26 23:31:22 +0800

    route

--- 6.node/express/my-express/app.js
+++ 6.node/express/my-express/app.js
@@ -3,18 +3,22 @@
  * - 基本实现
  */
 
-const express = require('./express')
+const express = require("./express");
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  res.end('get /')
-})
+app.get("/", (req, res) => {
+  res.end("get /");
+});
 
-app.get('/about', (req, res) => {
-  res.end('get /about')
-})
+app.get("/about", (req, res) => {
+  res.end("get /about");
+});
+
+app.get("/a{b}?cd/:c", (req, res) => {
+  res.end("get /ab?cd");
+});
 
 app.listen(3000, () => {
-  console.log('http://localhost:3000')
-})
+  console.log("http://localhost:3000");
+});
--- 6.node/express/my-express/express/lib/express.js
+++ 6.node/express/my-express/express/lib/express.js
@@ -1,35 +1,8 @@
-const http = require("http");
-const url = require("url");
-const routes = [
-  // { path: '', method: '', handler: () => {} }
-  // { path: '', method: '', handler: () => {} }
-  // { path: '', method: '', handler: () => {} }
-  // { path: '', method: '', handler: () => {} }
-];
-const createApplication = () => {
-  return {
-    get(path, handler) {
-      routes.push({
-        path,
-        method: "get",
-        handler,
-      });
-    },
-    listen: (...args) => {
-      const server = http.createServer((req, res) => {
-        const { pathname } = url.parse(req.url);
-        const method = req.method.toLowerCase();
-        const route = routes.find(
-          (route) => route.path === pathname && route.method === method
-        );
-        if (route) {
-          return route.handler(req, res);
-        }
-        res.end("404 Not Found.");
-      });
-      server.listen(...args);
-    },
-  };
-};
+const App = require('./application')
 
-module.exports = createApplication;
+function createApplication () {
+  const app = new App()
+  return app
+}
+
+module.exports = createApplication
