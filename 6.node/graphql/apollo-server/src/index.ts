4cb44c11ba48824db88c1bb3b223c0e38968f858
laoriy <1161612005@qq.com>
2024-09-15 23:23:36 +0800

    finish apollo server

--- 6.node/graphql/apollo-server/src/index.ts
+++ 6.node/graphql/apollo-server/src/index.ts
@@ -1,58 +1,52 @@
+import http from "http";
+import Koa from "koa";
+import bodyParser from "koa-bodyparser";
+import cors from "@koa/cors";
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { koaMiddleware } from "@as-integrations/koa";
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
+// The GraphQL schema
 const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    books: [Book]
+    hello: String
+    numberSix: Int
+    numberSeven: Int
   }
+  
 `;
 
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass",
-    author: "Paul Auster",
-  },
-];
-
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
+// A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    books: () => books,
+    hello: () => "world",
+    numberSix() {
+      return 6;
+    },
+    numberSeven() {
+      return 7;
+    },
   },
 };
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
+const app = new Koa();
+const httpServer = http.createServer(app.callback());
+
+// Set up Apollo Server
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
-  
-  // Passing an ApolloServer instance to the `startStandaloneServer` function:
-  //  1. creates an Express app
-  //  2. installs your ApolloServer instance as middleware
-  //  3. prepares your app to handle incoming requests
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-  });
-  
-  console.log(`🚀  Server ready at: ${url}`);
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+await server.start();
+
+app.use(cors());
+app.use(bodyParser());
+app.use(
+  koaMiddleware(server, {
+    context: async ({ ctx }) => ({ token: ctx.headers.token }),
+  })
+);
+
+await new Promise((resolve: any) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Server ready at http://localhost:4000`);
