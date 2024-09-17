8ae3dce325f67b60ff5eae16e4b47acdee1cae2b
laoriy <1161612005@qq.com>
2024-09-17 13:55:27 +0800

    feat: egg start

---
+++ 6.node/egg/egg-example/app/controller/home.js
@@ -0,0 +1,19 @@
+const Controller = require("egg").Controller;
+
+class HomeController extends Controller {
+  async index() {
+    this.ctx.body = "Hello world";
+  }
+  async list() {
+    const dataList = {
+        list: [
+          { id: 1, title: 'This is news 1', url: '/news/1' },
+          { id: 2, title: 'This is news 2', url: '/news/2' }
+        ]
+      };
+
+    await this.ctx.render("index.tpl", dataList);
+  }
+}
+
+module.exports = HomeController;
---
+++ 6.node/egg/egg-example/config/config.default.js
@@ -0,0 +1,9 @@
+exports.keys = "laor egg";
+
+// 添加 view 配置项
+exports.view = {
+  defaultViewEngine: "nunjucks",
+  mapping: {
+    ".tpl": "nunjucks",
+  },
+};
