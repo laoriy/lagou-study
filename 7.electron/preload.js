8bf759f0802bc70b07c9cc161243af8277871b2e
laoriy <1161612005@qq.com>
2024-10-29 23:16:47 +0800

    feat: open window

--- 7.electron/preload.js
+++ 7.electron/preload.js
@@ -1,3 +1,5 @@
+const { BrowserWindow } = require("@electron/remote");
+
 window.addEventListener("DOMContentLoaded", () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector);
@@ -7,4 +9,18 @@ window.addEventListener("DOMContentLoaded", () => {
   for (const dependency of ["chrome", "node", "electron"]) {
     replaceText(`${dependency}-version`, process.versions[dependency]);
   }
+  // 点击按钮打开一个新窗口
+  const oBtn = document.getElementById("btn");
+  oBtn.addEventListener("click", () => {
+    // ?? 如何去创建窗口
+    let indexMin = new BrowserWindow({
+      width: 200,
+      height: 200,
+    });
+    indexMin.loadFile("list.html");
+
+    indexMin.on("close", () => {
+      indexMin = null;
+    });
+  });
 });
