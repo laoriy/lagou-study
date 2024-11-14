827a44d84979e7aba5e8638bca8bd49c119bfe84
laoriy <1161612005@qq.com>
2024-11-14 23:31:57 +0800

    start

--- 8.electron-app/src/App.js
+++ 8.electron-app/src/App.js
@@ -1,21 +1,29 @@
-import "./App.css";
+import React from "react";
+import styled from "styled-components";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// 自定义左侧容器
+let LeftDiv = styled.div.attrs({
+  className: "col-3 left-panel",
+})`
+  background-color: #7b8c7c;
+  min-height: 100vh;
+`;
+
+// 自定义右侧容器
+let RightDiv = styled.div.attrs({
+  className: "col-9 right-panel",
+})`
+  background-color: #c9d8cd;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container-fluid px-0">
+      <div className="row g-0">
+        <LeftDiv>左侧</LeftDiv>
+        <RightDiv>右侧</RightDiv>
+      </div>
     </div>
   );
 }
