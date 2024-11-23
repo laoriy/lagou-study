21d08b2fc1d8d3c912e40141fe90c744e3b55798
laoriy <1161612005@qq.com>
2024-11-23 23:23:45 +0800

    action

--- 8.electron-app/src/App.js
+++ 8.electron-app/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import "bootstrap/dist/css/bootstrap.min.css";
 import SearchFile from "./components/SearchFile";
@@ -42,9 +42,71 @@ let RightDiv = styled.div.attrs({
   className: "col-9 right-panel",
 })`
   background-color: #c9d8cd;
+  .init-page {
+    color: #888;
+    text-align: center;
+    font: normal 28px/300px "微软雅黑";
+  }
 `;
 
 function App() {
+  const [files, setFiles] = useState(initFiles); // 代表所有的文件信息
+  const [activeId, setActiveId] = useState(""); // 当前正在编辑的文件id
+  const [openIds, setOpenIds] = useState([]); // 当前已打开的所有文件信息 ids
+  const [unSaveIds, setUnSaveIds] = useState([]); // 当前未被保存的所有文件信息 ids
+
+  // 计算已打开的所有文件信息
+  const openFiles = openIds.map((openId) => {
+    return files.find((file) => file.id === openId);
+  });
+
+  // 计算正在编辑的文件信息
+  const activeFile = files.find((file) => file.id === activeId);
+
+  // 01 点击左侧文件显示编辑页
+  const openItem = (id) => {
+    // 将当前 id 设置为 active id
+    setActiveId(id);
+
+    // 将id添加至 open ids
+    if (!openIds.includes(id)) {
+      setOpenIds([...openIds, id]);
+    }
+  };
+  // 02 点击某个选项时切换当前状态
+  const changeActive = (id) => {
+    setActiveId(id);
+  };
+
+  // 03 点击关闭按钮
+  const closeFile = (id) => {
+    // 将当前的 id 从已经 open 的数组中去除
+    const retOpen = openIds.filter((openId) => openId !== id);
+    setOpenIds(retOpen);
+    // 当某一个选项被关闭之后还需要给所有已打开文件设置一个当前状态
+    if (retOpen.length > 0) {
+      setActiveId(retOpen[0]);
+    } else {
+      setActiveId("");
+    }
+  };
+
+  // 04 当文件内容更新时
+  const changeFile = (id, newValue) => {
+    if (!unSaveIds.includes(id)) {
+      setUnSaveIds([...unSaveIds, id]);
+    }
+
+    // 某个内容更新之后我们需要生成新的 files
+    const newFiles = files.map((file) => {
+      if (file.id === id) {
+        file.body = newValue;
+      }
+      return file;
+    });
+    setFiles(newFiles);
+  };
+
   return (
     <div className="App container-fluid px-0">
       <div className="row g-0">
@@ -57,9 +119,7 @@ function App() {
           ></SearchFile>
           <FileList
             files={initFiles}
-            editFile={(id) => {
-              console.log(id);
-            }}
+            editFile={openItem}
             deleteFile={(id) => {
               console.log("删除", id);
             }}
@@ -73,29 +133,32 @@ function App() {
           </div>
         </LeftDiv>
         <RightDiv>
-          <TabList
-            files={initFiles}
-            activeItem={"1"}
-            unSaveItems={["1", "2"]}
-            clickItem={(id) => {
-              console.log(id);
-            }}
-            closeItem={(id) => {
-              console.log("关闭", id);
-            }}
-          />
-          <SimpleMDE
-            id="your-custom-id"
-            onChange={(value) => {
-              console.log(value);
-            }}
-            value={initFiles[1].body}
-            options={{
-              autofocus: true,
-              spellChecker: false,
-              minHeight: "445px",
-            }}
-          />
+          {activeFile && (
+            <>
+              <TabList
+                files={openFiles}
+                activeItem={activeId}
+                unSaveItems={unSaveIds}
+                clickItem={changeActive}
+                closeItem={closeFile}
+              />
+              <SimpleMDE
+                id="your-custom-id"
+                onChange={(value) => {
+                  changeFile(activeFile.id, value);
+                }}
+                value={activeFile.body}
+                options={{
+                  autofocus: true,
+                  spellChecker: false,
+                  minHeight: "445px",
+                }}
+              />
+            </>
+          )}
+          {!activeFile && (
+            <div className="init-page">新建或者导入具体的文档</div>
+          )}
         </RightDiv>
       </div>
     </div>
