6d39f69ee44a4e78a1533f04e3cdc8700a13ece8
laoriy <1161612005@qq.com>
2024-12-08 23:46:22 +0800

    single react

--- 9.微前端/2.single-spa/container/src/laoriy-root-config.js
+++ 9.微前端/2.single-spa/container/src/laoriy-root-config.js
@@ -1,24 +1,41 @@
 import { registerApplication, start } from "single-spa";
 
-registerApplication({
-  name: "@single-spa/welcome", // 微应用的名称
-  app: () =>
-    import(
-      /* webpackIgnore: true */
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-    ),
-  activeWhen: ["/"], // 激活的路由
-});
-
 // registerApplication({
-//   name: "@laoriy/navbar",
+//   name: "@single-spa/welcome", // 微应用的名称
 //   app: () =>
 //     import(
 //       /* webpackIgnore: true */
-//       "@laoriy/navbar"
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
 //     ),
-//   activeWhen: ["/"],
+//   activeWhen: ["/"], // 激活的路由
 // });
+
+registerApplication(
+  "@single-spa/welcome",
+  () =>
+    import("https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"),
+  (location) => location.pathname === "/"
+);
+
+registerApplication({
+  name: "@laoriy/lagou",
+  app: () =>
+    import(
+      /* webpackIgnore: true */
+      "@laoriy/lagou"
+    ),
+  activeWhen: ["/lagou"],
+});
+
+registerApplication({
+  name: "@laoriy/todos",
+  app: () =>
+    import(
+      /* webpackIgnore: true */
+      "@laoriy/todos"
+    ),
+  activeWhen: ["/todos"],
+});
 
 start({
   urlRerouteOnly: true,
