36e670793889a6879422cb622d56e7c41039bb54
laoriy <1161612005@qq.com>
2024-12-18 23:42:39 +0800

    single-spa-layout

--- 9.微前端/2.single-spa/container/src/laoriy-root-config.js
+++ 9.微前端/2.single-spa/container/src/laoriy-root-config.js
@@ -1,4 +1,25 @@
 import { registerApplication, start } from "single-spa";
+import {
+  constructApplications,
+  constructRoutes,
+  constructLayoutEngine,
+} from "single-spa-layout";
+console.log(document.querySelector("#single-spa-layout"));
+
+const routes = constructRoutes(document.querySelector("#single-spa-layout"));
+const applications = constructApplications({
+  routes,
+  loadApp({ name }) {
+    return System.import(name);
+  },
+});
+const layoutEngine = constructLayoutEngine({ routes, applications });
+
+applications.forEach(registerApplication);
+
+/**
+ * 以下方式是没有使用single-spa-layout的方式
+ */
 
 // registerApplication({
 //   name: "@single-spa/welcome", // 微应用的名称
@@ -10,40 +31,40 @@ import { registerApplication, start } from "single-spa";
 //   activeWhen: ["/"], // 激活的路由
 // });
 
-registerApplication(
-  "@single-spa/welcome",
-  () =>
-    System.import(
-      "https://cdn.jsdelivr.net/npm/single-spa-welcome@2.4.3/dist/single-spa-welcome.js"
-    ),
-  (location) => location.pathname === "/"
-);
+// registerApplication(
+//   "@single-spa/welcome",
+//   () =>
+//     System.import(
+//       "https://cdn.jsdelivr.net/npm/single-spa-welcome@2.4.3/dist/single-spa-welcome.js"
+//     ),
+//   (location) => location.pathname === "/"
+// );
 
-registerApplication({
-  name: "@laoriy/lagou",
-  app: () =>
-    System.import(
-      /* webpackIgnore: true */
-      "@laoriy/lagou"
-    ),
-  activeWhen: ["/lagou"],
-});
+// registerApplication({
+//   name: "@laoriy/lagou",
+//   app: () =>
+//     System.import(
+//       /* webpackIgnore: true */
+//       "@laoriy/lagou"
+//     ),
+//   activeWhen: ["/lagou"],
+// });
 
-registerApplication({
-  name: "@laoriy/todos",
-  app: () =>
-    System.import(
-      /* webpackIgnore: true */
-      "@laoriy/todos"
-    ),
-  activeWhen: ["/todos"],
-});
+// registerApplication({
+//   name: "@laoriy/todos",
+//   app: () =>
+//     System.import(
+//       /* webpackIgnore: true */
+//       "@laoriy/todos"
+//     ),
+//   activeWhen: ["/todos"],
+// });
 
-registerApplication({
-  name: "@laoriy/realworld",
-  app: () => System.import("@laoriy/realworld"),
-  activeWhen: ["/realworld"],
-});
+// registerApplication({
+//   name: "@laoriy/realworld",
+//   app: () => System.import("@laoriy/realworld"),
+//   activeWhen: ["/realworld"],
+// });
 
 start({
   urlRerouteOnly: true,
