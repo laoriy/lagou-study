19b5766e7e5fc0d698c005ebc1bba7b4369a80c0
laoriy <1161612005@qq.com>
2025-01-01 23:18:55 +0800

    start application

---
+++ 9.微前端/4.applications/marketing/src/App.js
@@ -0,0 +1,24 @@
+import React from "react"
+// import { Router, Route, Switch } from "react-router-dom"
+// import Landing from "./components/Landing"
+// import Pricing from "./components/Pricing"
+
+function App({ history }) {
+  return (
+    // <Router history={history}>
+    //   <Switch>
+    //     <Route path="/pricing">
+    //       <Pricing />
+    //     </Route>
+    //     <Route path="/">
+    //       <Landing />
+    //     </Route>
+    //   </Switch>
+    // </Router>
+    <div>
+      hello world
+    </div>
+  )
+}
+
+export default App
