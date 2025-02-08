2b5383db2ea86a3eba657226a1cbf3ca2e785ac4
laoriy <1161612005@qq.com>
2025-02-08 23:53:41 +0800

    some test

--- 10.自动化测试/vitest/src/components/TodoApp/__tests__/TodoItem.spec.ts
+++ 10.自动化测试/vitest/src/components/TodoApp/__tests__/TodoItem.spec.ts
@@ -49,32 +49,32 @@ describe('TodoItem.vue', () => {
     expect(todoItem.classes('editing')).toBeFalsy()
   })
 
-  // test('save edit todo', async () => {
-  //   const label = wrapper.find('label[data-testid="todo-text"]')
-  //   const todoEdit = wrapper.find('input[data-testid="todo-edit"]')
-  //   await label.trigger('dblclick')
+  test('save edit todo', async () => {
+    const label = wrapper.find('label[data-testid="todo-text"]')
+    const todoEdit = wrapper.find('input[data-testid="todo-edit"]')
+    await label.trigger('dblclick')
 
-  //   // 编辑文本框中的内容展示
-  //   expect(todoEdit.element.value).toBe(wrapper.vm.todo.text)
+    // 编辑文本框中的内容展示
+    expect(todoEdit.element.value).toBe(wrapper.vm.todo.text)
 
-  //   // 修改文本框的值
-  //   const text = 'hello'
-  //   await todoEdit.setValue(text)
+    // 修改文本框的值
+    const text = 'hello'
+    await todoEdit.setValue(text)
 
-  //   // 触发回车保存事件
-  //   await todoEdit.trigger('keyup.enter')
+    // 触发回车保存事件
+    await todoEdit.trigger('keyup.enter')
 
-  //   // 断言数据被修改了
-  //   // expect(wrapper.vm.todo.text).toBe(text)
-  //   expect(wrapper.emitted()['edit-todo']).toBeTruthy()
-  //   expect(wrapper.emitted()['edit-todo'][0][0]).toEqual({
-  //     id: wrapper.vm.todo.id,
-  //     text
-  //   })
+    // 断言数据被修改了
+    // expect(wrapper.vm.todo.text).toBe(text)
+    expect(wrapper.emitted()['edit-todo']).toBeTruthy()
+    expect(wrapper.emitted()['edit-todo'][0][0]).toEqual({
+      id: wrapper.vm.todo.id,
+      text
+    })
 
-  //   // 确保取消了编辑状态
-  //   expect(wrapper.vm.isEditing).toBeFalsy()
-  // })
+    // 确保取消了编辑状态
+    expect(wrapper.vm.isEditing).toBeFalsy()
+  })
 
   // test('cancel edit todo', async () => {
   //   const label = wrapper.find('label[data-testid="todo-text"]')
